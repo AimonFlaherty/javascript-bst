d97b228dd38ab13a66529ec003689b271cc5b573
aimon flaherty <aimonflaherty@gmail.com>
2023-03-31 12:03:11 -0400

    added test for add/inorder and size, finished insert, size & inorder, added Readme

--- src/bst.js
+++ src/bst.js
@@ -12,9 +12,41 @@ class Bst{
         this.root = null;
     }
     insert(data){
-        if(this.root == null) this.root = new Node(data);
+        
+        function recursiveInsert(node, data){
+            if(node == null){
+                return new Node(data);
+            }
+            else if(node.data < data) node.right = recursiveInsert(node.right, data);
+            else if(node.data > data) node.left = recursiveInsert(node.left, data);
+            return node;
+        }
+        this.root = recursiveInsert(this.root, data);
         
     }
+    size(){
+        function traverse(node){
+            let sum = 0;
+            if(node == null) return 0;
+            sum = sum + traverse(node.left);
+            sum = sum + traverse(node.right);
+            return sum + 1;
+        }
+        return traverse(this.root);
+    }
+    inOrder(){
+        const list =[];
+        return this.inorderRecursive(this.root, list);
+    }
+    inorderRecursive(node, list){
+        if(node == null) return list;
+        list = this.inorderRecursive(node.left, list);
+        list.push(node.data);
+        list = this.inorderRecursive(node.right, list);
+        return list;
+    }
+    
 }
+
 //export default Bst;
 module.exports = Bst;
--- tests/bstTest.test.js
+++ tests/bstTest.test.js
@@ -7,4 +7,19 @@ describe("Test insert", ()=>{
         tree.insert(9);
         expect(tree.root.data).toEqual(9);
     });
+    it("Should add all numbers to the bst", ()=>{
+        const input = [2, 1, 3];
+        const output = [1, 2, 3];
+        const tree = new Bst();
+        input.forEach((num)=>tree.insert(num));
+        expect(tree.inOrder()).toEqual(output);
+
+    });
+    it("Should return the correct size of the bst", ()=>{
+        const values = [...Array(5).keys()];
+        const tree = new Bst();
+        values.forEach((val)=> tree.insert(val));
+        console.log(tree.root.data, tree.root.left, tree.root.right);
+        expect(tree.size()).toEqual(5);
+    });
 });
